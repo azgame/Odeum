2027aa8043497389f24d9563987206065b7159a0
azizys <36770253+azizys@users.noreply.github.com>
2019-10-01 14:12:08 -0400

    adding camera class

--- Odeum/OdeumWin32/Camera.cpp
+++ Odeum/OdeumWin32/Camera.cpp
@@ -10,3 +10,19 @@ Camera::Camera()
 Camera::~Camera()
 {
 }
+
+void Camera::SetViewMatrix(DirectX::XMFLOAT3 eye, DirectX::XMFLOAT3 lookAt, DirectX::XMFLOAT3 up)
+{
+}
+
+void Camera::SetProjMatrix(float fieldOfView, float aspectRatio, float nearPlane, float farPlane)
+{
+}
+
+void Camera::LookDirection(DirectX::XMFLOAT3 lookDirection)
+{
+}
+
+void Camera::Eye(DirectX::XMFLOAT3 position)
+{
+}
--- Odeum/OdeumWin32/Camera.h
+++ Odeum/OdeumWin32/Camera.h
@@ -1,8 +1,45 @@
-#pragma once
+#ifndef _CAMERA_H_
+#define _CAMERA_H_
+
+#include "pch.h"
+
 class Camera
 {
 public:
+
 	Camera();
 	~Camera();
+
+	void SetViewMatrix(DirectX::XMFLOAT3 eye, DirectX::XMFLOAT3 lookAt, DirectX::XMFLOAT3 up);
+	void SetProjMatrix(float fieldOfView, float aspectRatio, float nearPlane, float farPlane);
+
+	void LookDirection(DirectX::XMFLOAT3 lookDirection);
+	void Eye(DirectX::XMFLOAT3 position);
+
+
+
+	float NearClipPlane();
+	float FarClipPlane();
+	float Pitch();
+	float Yaw();
+
+private:
+	DirectX::XMFLOAT4X4 m_viewMatrix;
+	DirectX::XMFLOAT4X4 m_projectionMatrix;
+	DirectX::XMFLOAT4X4 m_projectionMatrixLeft;
+	DirectX::XMFLOAT4X4 m_projectionMatrixRight;
+
+	DirectX::XMFLOAT4X4 m_inverseView;
+
+	DirectX::XMFLOAT3 m_eye;
+	DirectX::XMFLOAT3 m_lookAt;
+	DirectX::XMFLOAT3 m_up;
+
+	float			m_pitch, m_yaw, m_roll;
+	float           m_fieldOfView;
+	float           m_aspectRatio;
+	float           m_nearPlane;
+	float           m_farPlane;
 };
 
+#endif // !_CAMERA_H_
