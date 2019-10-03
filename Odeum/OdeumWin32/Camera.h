73ccd97a62a2b022195a7db7d3792e9c6b68af34
azizys <36770253+azizys@users.noreply.github.com>
2019-10-03 11:46:55 -0400

    updates to camera

--- Odeum/OdeumWin32/Camera.h
+++ Odeum/OdeumWin32/Camera.h
@@ -16,30 +16,28 @@ public:
 	void LookDirection(DirectX::XMFLOAT3 lookDirection);
 	void Eye(DirectX::XMFLOAT3 position);
 
-
-
-	float NearClipPlane();
-	float FarClipPlane();
-	float Pitch();
-	float Yaw();
+	float NearClipPlane()				{ return m_nearPlane; }
+	float FarClipPlane()				{ return m_farPlane; }
+	float Pitch()						{ return m_pitch; }
+	float Yaw()							{ return m_yaw; }
 
 private:
-	DirectX::XMFLOAT4X4 m_viewMatrix;
-	DirectX::XMFLOAT4X4 m_projectionMatrix;
-	DirectX::XMFLOAT4X4 m_projectionMatrixLeft;
-	DirectX::XMFLOAT4X4 m_projectionMatrixRight;
+	DirectX::XMFLOAT4X4					m_viewMatrix;
+	DirectX::XMFLOAT4X4					m_projectionMatrix;
+	DirectX::XMFLOAT4X4					m_projectionMatrixLeft;
+	DirectX::XMFLOAT4X4					m_projectionMatrixRight;
 
-	DirectX::XMFLOAT4X4 m_inverseView;
+	DirectX::XMFLOAT4X4					m_inverseView;
 
-	DirectX::XMFLOAT3 m_eye;
-	DirectX::XMFLOAT3 m_lookAt;
-	DirectX::XMFLOAT3 m_up;
+	DirectX::XMFLOAT3					m_eye;
+	DirectX::XMFLOAT3					m_lookAt;
+	DirectX::XMFLOAT3					m_up;
 
-	float			m_pitch, m_yaw, m_roll;
-	float           m_fieldOfView;
-	float           m_aspectRatio;
-	float           m_nearPlane;
-	float           m_farPlane;
+	float								m_pitch, m_yaw, m_roll;
+	float								m_fieldOfView;
+	float								m_aspectRatio;
+	float								m_nearPlane;
+	float								m_farPlane;
 };
 
 #endif // !_CAMERA_H_
