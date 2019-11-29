5b277817e8050683f1a15bf92f30d3d6a4603393
azizys <36770253+azizys@users.noreply.github.com>
2019-11-29 14:20:11 -0500

    updated gitignore for dll inclusion

--- Odeum/OdeumWin32/Miss.hlsl
+++ Odeum/OdeumWin32/Miss.hlsl
@@ -3,9 +3,9 @@
 [shader("miss")]
 void MyMissShader(inout RayPayload payload)
 {
-	uint2 launchIndex = DispatchRaysIndex();
-	float2 dims = float2(DispatchRaysDimensions());
+	uint2 launchIndex = DispatchRaysIndex().xy;
+	float2 dims = float2(DispatchRaysDimensions().xy);
 
 	float ramp = launchIndex.y / dims.y;
-	payload.color = float4(0.0f, 0.2f, 0.7f - 0.3f*ramp, 1.0f);
+	payload.color = float4(0.0f, 0.2f, 0.7f - 0.5f*ramp, -1.0f);
 }
