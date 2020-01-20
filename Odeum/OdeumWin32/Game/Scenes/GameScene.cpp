3d6993ff87698a5fd782ee0d73ee569bbfa0536e
azgame <azizysgame@gmail.com>
2020-01-20 18:50:34 -0500

    relative camera movement and multiple objects dx12 base

--- Odeum/OdeumWin32/Game/Scenes/GameScene.cpp
+++ Odeum/OdeumWin32/Game/Scenes/GameScene.cpp
@@ -13,12 +13,13 @@ GameScene::~GameScene()
 
 bool GameScene::Initialize()
 {
-	Debug::Info("Creating Game Scene", __FILE__, __LINE__);
+	Debug::Info("Creating Game Scene", __FILENAME__, __LINE__);
 	return true;
 }
 
 void GameScene::Update(const float deltaTime_)
 {
+	Debug::Info(std::to_string(deltaTime_), __FILENAME__, __LINE__);
 }
 
 void GameScene::Render()
