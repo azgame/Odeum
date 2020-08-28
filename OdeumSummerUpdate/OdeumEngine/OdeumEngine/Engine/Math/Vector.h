2d6e7c589c79d3c920da70399d9b47d3f4e10fa2
Ryan Thorne <rymaster_109@hotmail.com>
2020-08-28 07:32:34 -0400

    Renaming and working on Vec4s
    Vector4 isnt finished yet but vec3 should be fully functional

--- OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Vector.h
+++ OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Vector.h
@@ -1,7 +1,7 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-// Many of these functions were adapted from the DirectX graphics samples github repo
+// Many of these functions were adapted from the DirectX graphics samples github repo.
 
 #include "../../pch.h"
 
@@ -14,26 +14,26 @@ public:
 	inline Scalar(float f_) { vec = DirectX::XMVectorReplicate(f_); }
 	// Create a new Scalar from an existing FXMVector
 	inline explicit Scalar(DirectX::FXMVECTOR vec_) { vec = vec_; }
-	// returns the XMVECTOR, I had to create this function because XMVector functions can't have a scalar given to them
+	// returns the XMVECTOR, I had to create this function because XMVector functions can't have a scalar given to them.
 	inline DirectX::XMVECTOR GetVec() { return vec; }
 private:
 	DirectX::XMVECTOR vec;
 };
 
-// Operator overloads for Scalars, using XMVector functions and overloads
-inline Scalar operator- (Scalar s) { return Scalar(DirectX::XMVectorNegate(s.GetVec())); }
-inline Scalar operator+ (Scalar s1, Scalar s2) { return Scalar(DirectX::XMVectorAdd(s1.GetVec(), s2.GetVec())); }
-inline Scalar operator- (Scalar s1, Scalar s2) { return Scalar(DirectX::XMVectorSubtract(s1.GetVec(), s2.GetVec())); }
-inline Scalar operator* (Scalar s1, Scalar s2) { return Scalar(DirectX::XMVectorMultiply(s1.GetVec(), s2.GetVec())); }
-inline Scalar operator/ (Scalar s1, Scalar s2) { return Scalar(DirectX::XMVectorDivide(s1.GetVec(), s2.GetVec())); }
-inline Scalar operator+ (Scalar s1, float s2) { return s1 + Scalar(s2); }
-inline Scalar operator- (Scalar s1, float s2) { return s1 - Scalar(s2); }
-inline Scalar operator* (Scalar s1, float s2) { return s1 * Scalar(s2); }
-inline Scalar operator/ (Scalar s1, float s2) { return s1 / Scalar(s2); }
-inline Scalar operator+ (float s1, Scalar s2) { return Scalar(s1) + s2; }
-inline Scalar operator- (float s1, Scalar s2) { return Scalar(s1) - s2; }
-inline Scalar operator* (float s1, Scalar s2) { return Scalar(s1) * s2; }
-inline Scalar operator/ (float s1, Scalar s2) { return Scalar(s1) / s2; }
+// Operator overloads for Scalars, using XMVector functions and overloads.
+inline Scalar operator- (Scalar s_) { return Scalar(DirectX::XMVectorNegate(s_.GetVec())); }
+inline Scalar operator+ (Scalar s1_, Scalar s2_) { return Scalar(DirectX::XMVectorAdd(s1_.GetVec(), s2_.GetVec())); }
+inline Scalar operator- (Scalar s1_, Scalar s2_) { return Scalar(DirectX::XMVectorSubtract(s1_.GetVec(), s2_.GetVec())); }
+inline Scalar operator* (Scalar s1_, Scalar s2_) { return Scalar(DirectX::XMVectorMultiply(s1_.GetVec(), s2_.GetVec())); }
+inline Scalar operator/ (Scalar s1_, Scalar s2_) { return Scalar(DirectX::XMVectorDivide(s1_.GetVec(), s2_.GetVec())); }
+inline Scalar operator+ (Scalar s1_, float s2_) { return s1_ + Scalar(s2_); }
+inline Scalar operator- (Scalar s1_, float s2_) { return s1_ - Scalar(s2_); }
+inline Scalar operator* (Scalar s1_, float s2_) { return s1_ * Scalar(s2_); }
+inline Scalar operator/ (Scalar s1_, float s2_) { return s1_ / Scalar(s2_); }
+inline Scalar operator+ (float s1_, Scalar s2_) { return Scalar(s1_) + s2_; }
+inline Scalar operator- (float s1_, Scalar s2_) { return Scalar(s1_) - s2_; }
+inline Scalar operator* (float s1_, Scalar s2_) { return Scalar(s1_) * s2_; }
+inline Scalar operator/ (float s1_, Scalar s2_) { return Scalar(s1_) / s2_; }
 
 
 
@@ -45,36 +45,50 @@ public:
 	inline Vector3(const DirectX::XMFLOAT3& v_) { vec = XMLoadFloat3(&v_); }
 	inline Vector3(const DirectX::XMVECTOR& v_) { vec = v_; }
 	inline Vector3(Scalar s) { vec = s.GetVec(); }
-	inline DirectX::XMVECTOR GetVec() { return vec; }
 	
 	// Getters and setters
+	inline DirectX::XMVECTOR GetVec() { return vec; }
 	inline Scalar GetX() const { return Scalar(DirectX::XMVectorSplatX(vec)); }
 	inline Scalar GetY() const { return Scalar(DirectX::XMVectorSplatY(vec)); }
 	inline Scalar GetZ() const { return Scalar(DirectX::XMVectorSplatZ(vec)); }
-	inline void SetX(Scalar x) { vec = DirectX::XMVectorPermute<4, 1, 2, 3>(vec, x.GetVec()); }
-	inline void SetY(Scalar y) { vec = DirectX::XMVectorPermute<0, 5, 2, 3>(vec, y.GetVec()); }
-	inline void SetZ(Scalar z) { vec = DirectX::XMVectorPermute<0, 1, 6, 3>(vec, z.GetVec()); }
+	inline void SetX(Scalar x_) { vec = DirectX::XMVectorPermute<4, 1, 2, 3>(vec, x_.GetVec()); }
+	inline void SetY(Scalar y_) { vec = DirectX::XMVectorPermute<0, 5, 2, 3>(vec, y_.GetVec()); }
+	inline void SetZ(Scalar z_) { vec = DirectX::XMVectorPermute<0, 1, 6, 3>(vec, z_.GetVec()); }
 
+	// Operator overloads
 	inline Vector3 operator- () const { return Vector3(DirectX::XMVectorNegate(vec)); }
-	inline Vector3 operator+ (Vector3 v2) const { return Vector3(DirectX::XMVectorAdd(vec, v2.GetVec())); }
-	inline Vector3 operator- (Vector3 v2) const { return Vector3(DirectX::XMVectorSubtract(vec, v2.GetVec())); }
-	inline Vector3 operator* (Vector3 v2) const { return Vector3(DirectX::XMVectorMultiply(vec, v2.GetVec())); }
-	inline Vector3 operator/ (Vector3 v2) const { return Vector3(DirectX::XMVectorDivide(vec, v2.GetVec())); }
-	inline Vector3 operator* (Scalar  v2) const { return *this * Vector3(v2); }
-	inline Vector3 operator/ (Scalar  v2) const { return *this / Vector3(v2); }
-	inline Vector3 operator* (float  v2) const { return *this * Scalar(v2); }
-	inline Vector3 operator/ (float  v2) const { return *this / Scalar(v2); }
+	inline Vector3 operator+ (Vector3 v2_) const { return Vector3(DirectX::XMVectorAdd(vec, v2_.GetVec())); }
+	inline Vector3 operator- (Vector3 v2_) const { return Vector3(DirectX::XMVectorSubtract(vec, v2_.GetVec())); }
+	inline Vector3 operator* (Vector3 v2_) const { return Vector3(DirectX::XMVectorMultiply(vec, v2_.GetVec())); }
+	inline Vector3 operator/ (Vector3 v2_) const { return Vector3(DirectX::XMVectorDivide(vec, v2_.GetVec())); }
+	inline Vector3 operator* (Scalar  v2_) const { return *this * Vector3(v2_); }
+	inline Vector3 operator/ (Scalar  v2_) const { return *this / Vector3(v2_); }
+	inline Vector3 operator* (float  v2_) const { return *this * Scalar(v2_); }
+	inline Vector3 operator/ (float  v2_) const { return *this / Scalar(v2_); }
 
-	inline Vector3& operator += (Vector3 v) { *this = *this + v; return *this; }
-	inline Vector3& operator -= (Vector3 v) { *this = *this - v; return *this; }
-	inline Vector3& operator *= (Vector3 v) { *this = *this * v; return *this; }
-	inline Vector3& operator /= (Vector3 v) { *this = *this / v; return *this; }
+	inline Vector3& operator += (Vector3 v_) { *this = *this + v_; return *this; }
+	inline Vector3& operator -= (Vector3 v_) { *this = *this - v_; return *this; }
+	inline Vector3& operator *= (Vector3 v_) { *this = *this * v_; return *this; }
+	inline Vector3& operator /= (Vector3 v_) { *this = *this / v_; return *this; }
 
-	inline friend Vector3 operator* (Scalar  v1, Vector3 v2) { return Vector3(v1) * v2; }
-	inline friend Vector3 operator/ (Scalar  v1, Vector3 v2) { return Vector3(v1) / v2; }
-	inline friend Vector3 operator* (float   v1, Vector3 v2) { return Scalar(v1) * v2; }
-	inline friend Vector3 operator/ (float   v1, Vector3 v2) { return Scalar(v1) / v2; }
+	inline friend Vector3 operator* (Scalar  v1_, Vector3 v2_) { return Vector3(v1_) * v2_; }
+	inline friend Vector3 operator/ (Scalar  v1_, Vector3 v2_) { return Vector3(v1_) / v2_; }
+	inline friend Vector3 operator* (float   v1_, Vector3 v2_) { return Scalar(v1_) * v2_; }
+	inline friend Vector3 operator/ (float   v1_, Vector3 v2_) { return Scalar(v1_) / v2_; }
 
+protected:
+	DirectX::XMVECTOR vec;
+};
+
+class Vector4 {
+public:
+	inline Vector4() { vec = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f); }
+	inline Vector4(float x_, float y_, float z_, float w_) { vec = DirectX::XMVectorSet(x_, y_, z_, w_); }
+	inline Vector4(Vector3 vec_, float w_) { vec = DirectX::XMVectorSetW(vec_.GetVec(), w_); }
+	inline Vector4(const Vector4& v_) { vec = v_.GetVec(); };
+	inline Vector4(const Scalar& s_) { vec = s_.GetVec(); };
+
+	inline DirectX::XMVECTOR GetVec() { return vec; }
 protected:
 	DirectX::XMVECTOR vec;
 };
