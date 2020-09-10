72c150be901892c3d783f9b58b46726fe691ce5a
BenSayliss <bensayliss@gmail.com>
2020-09-09 22:15:50 -0400

    Finished Vector4 and Quaternion, Working on Matrix3 and Matrix4
    Finished Vector4 and Quaternion, Working on Matrix3 and Matrix4

--- OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Matrix.h
+++ OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Matrix.h
@@ -1,12 +1,40 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-class Matrix3 
+// Many of these functions were adapted from the DirectX graphics samples github repo.
+
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Developed by Minigraph
+//
+// Author:  James Stanard 
+//
+
+#include "Quaternion.h"
+// Represents a 3x3 matrix while occuping a 4x4 memory footprint.  The unused row and column are undefined but implicitly
+// (0, 0, 0, 1).  Constructing a Matrix4 will make those values explicit.
+	
+//vv   not sure if we want to incorporate _declspec(align(16)) or not
+_declspec(align(16))class Matrix3
+{
+public:
+	inline Matrix3() {}
+	inline Matrix3(Vector3 x_, Vector3 y_, Vector3 z_) { mat[0] = x_; mat[1] = y_; mat[2] = z_; }
+	inline Matrix3(const Matrix3& m_) { m[0] = m_.m[0]; m[1] = m_.m[1]; m[2] = m_.m[2]; }
+	inline Matrix3(Quaternion q_) { *this = Matrix3(DirectX::XMMatrixRotationQuaternion(q_));  }
+private:
+	Vector3 mat[3];
+};
+
+class Matrix4
 {
 };
 
-class Matrix4 
-{
-};
 
 #endif
--- OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Vector.h
+++ OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Math/Vector.h
@@ -3,6 +3,19 @@
 
 // Many of these functions were adapted from the DirectX graphics samples github repo.
 
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+// Developed by Minigraph
+//
+// Author:  James Stanard 
+//
+
 #include "../../pch.h"
 
 // made Scalar inside of Vector for ease of access but I might move Scalar to its own class if we end up using it more
@@ -45,7 +58,9 @@ public:
 	inline Vector3(const DirectX::XMFLOAT3& v_) { vec = XMLoadFloat3(&v_); }
 	inline Vector3(const DirectX::XMVECTOR& v_) { vec = v_; }
 	inline Vector3(Scalar s) { vec = s.GetVec(); }
-	
+
+	inline operator DirectX::XMVECTOR() const { return vec; }
+
 	// Getters and setters
 	inline DirectX::XMVECTOR GetVec() { return vec; }
 	inline Scalar GetX() const { return Scalar(DirectX::XMVectorSplatX(vec)); }
@@ -85,10 +100,56 @@ public:
 	inline Vector4() { vec = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f); }
 	inline Vector4(float x_, float y_, float z_, float w_) { vec = DirectX::XMVectorSet(x_, y_, z_, w_); }
 	inline Vector4(Vector3 vec_, float w_) { vec = DirectX::XMVectorSetW(vec_.GetVec(), w_); }
-	inline Vector4(Vector4& v_) { vec = v_.GetVec(); }
+	//inline Vector4(Vector4& v_) { vec = v_.GetVec(); }
+	inline Vector4(const DirectX::XMVECTOR& v_) { vec = v_; }
 	inline Vector4(Scalar& s_) { vec = s_.GetVec(); }
 
+	inline operator DirectX::XMVECTOR() const { return vec; }
+	
+	// Getters and Setters
 	inline DirectX::XMVECTOR GetVec() { return vec; }
+	inline Scalar GetX() const { return Scalar(DirectX::XMVectorSplatX(vec)); }
+	inline Scalar GetY() const { return Scalar(DirectX::XMVectorSplatY(vec)); }
+	inline Scalar GetZ() const { return Scalar(DirectX::XMVectorSplatZ(vec)); }
+	inline Scalar GetW() const { return Scalar(DirectX::XMVectorSplatW(vec)); }
+	inline void SetX(Scalar x_) { vec = DirectX::XMVectorPermute<4, 1, 2, 3>(vec, x_.GetVec()); }
+	inline void SetY(Scalar y_) { vec = DirectX::XMVectorPermute<0, 5, 2, 3>(vec, y_.GetVec()); }
+	inline void SetZ(Scalar z_) { vec = DirectX::XMVectorPermute<0, 1, 6, 3>(vec, z_.GetVec()); }
+	inline void SetW(Scalar w_) { vec = DirectX::XMVectorPermute<0, 1, 2, 7>(vec, w_.GetVec()); }
+
+	// Operator Overloads
+	inline Vector4 operator- () const { return Vector4(DirectX::XMVectorNegate(vec)); }
+	inline Vector4 operator+ (Vector4 v2_) const { return Vector4(DirectX::XMVectorAdd(vec, v2_.GetVec())); }
+	inline Vector4 operator- (Vector4 v2_) const { return Vector4(DirectX::XMVectorSubtract(vec, v2_.GetVec())); }
+	inline Vector4 operator* (Vector4 v2_) const { return Vector4(DirectX::XMVectorMultiply(vec, v2_.GetVec())); }
+	inline Vector4 operator/ (Vector4 v2_) const { return Vector4(DirectX::XMVectorDivide(vec, v2_.GetVec())); }
+	inline Vector4 operator* (Scalar v2_) const { return *this * Vector4(v2_); }
+	inline Vector4 operator/ (Scalar v2_) const { return *this / Vector4(v2_); }
+	inline Vector4 operator* (float v2_) const { return *this * Scalar(v2_); }
+	inline Vector4 operator/ (float v2_) const { return *this / Scalar(v2_); }
+
+	inline Vector4& operator += (Vector4 v_) { *this = *this + v_; return *this; }
+	inline Vector4& operator -= (Vector4 v_) { *this = *this - v_; return *this; }
+	inline Vector4& operator *= (Vector4 v_) { *this = *this * v_; return *this; }
+	inline Vector4& operator /= (Vector4 v_) { *this = *this / v_; return *this; }
+
+	inline friend Vector4 operator* (Scalar v1_, Vector4 v2_) { return Vector4(v1_) * v2_; }
+	inline friend Vector4 operator/ (Scalar v1_, Vector4 v2_) { return Vector4(v1_) / v2_; }
+	inline friend Vector4 operator* (float v1_, Vector4 v2_) { return Scalar(v1_) * v2_; }
+	inline friend Vector4 operator/ (float v1_, Vector4 v2_) { return Scalar(v1_) / v2_; }
+
+protected:
+	DirectX::XMVECTOR vec;
+};
+
+// vv   not sure if we wanted to include this 
+
+class BoolVector
+{
+public:
+	inline BoolVector(DirectX::FXMVECTOR vec_) { vec = vec_; }
+	inline operator DirectX::XMVECTOR() const { return vec; }
+
 protected:
 	DirectX::XMVECTOR vec;
 };
