ef82d43e15bcfa2451dfc161ec395b747ca5ebc6
BenSayliss <bensayliss@gmail.com>
2020-09-16 10:51:26 -0400

    Events
    Added Application, Key, and Mouse Events. Still need to incorporate input...
    There will be errors not being able to find KeyCode and MouseCode

--- OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Events/KeyEvent.h
+++ OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Events/KeyEvent.h
@@ -1,6 +1,68 @@
 #ifndef KEYEVENT_H
 #define KEYEVENT_H
 
+#include "Event.h"
 
+// still have to add input! (KeyCode)
+class KeyEvent : public Event
+{
+public:
+	KeyCode GetKeyCode() const { return keyCode; }
+	
+	EVENT_CATEGORY(EventCategoryKeyboard | EventCategoryInput)
+protected:
+	KeyEvent(const KeyCode keyCode_) : keyCode(keyCode_) {}
+	KeyCode keyCode;
+};
+
+class KeyPressedEvent : public KeyEvent
+{
+public:
+	KeyPressedEvent(const KeyCode keyCode_, const uint16_t repeatCount_)
+		: KeyEvent(keyCode_), repeatCount(repeatCount_) {}
+
+	uint16_t GetRepeatCount() const { return repeatCount; }
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "KeyPressedEvent: " << keyCode << " (" << repeatCount << " repeats)";
+		return ss.str();
+	}
+
+	EVENT_TYPE(KeyPressed)
+private:
+	uint16_t repeatCount;
+};
+
+class KeyReleasedEvent : public KeyEvent
+{
+public:
+	KeyReleasedEvent(const KeyCode keyCode_) : KeyEvent(keyCode_) {}
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "KeyReleasedEvent: " << keyCode;
+		return ss.str();
+	}
+
+	EVENT_TYPE(KeyReleased)
+};
+
+class KeyTypedEvent : public KeyEvent
+{
+public:
+	KeyTypedEvent(const KeyCode keyCode_) : KeyEvent(keyCode_) {}
+
+	std::string ToString() const override
+	{
+		std::stringstream sss;
+		ss << "KeyTypedEvent: " << keyCode;
+		return ss.str();
+	}
+
+	EVENT_TYPE(KeyTyped)
+};
 
 #endif
--- OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Events/MouseEvent.h
+++ OdeumSummerUpdate/OdeumEngine/OdeumEngine/Engine/Events/MouseEvent.h
@@ -1,6 +1,93 @@
 #ifndef MOUSEEVENT_H
 #define MOUSEEVENT_H
 
+#include "Event.h"
 
+// still have to add input! (MouseCode)
+class MouseMovedEvent : public Event
+{
+public:
+	MouseMovedEvent(const float x_, const float y_) : mouseX(x_), mouseY(y_) {}
 
+	// Getters
+	float GetX() const { return mouseX; }
+	float GetY() const { return mouseY; }
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "MouseMovedEvent: " << mouseX << ", " << mouseY;
+		return ss.str();
+	}
+
+	EVENT_TYPE(MouseMoved)
+	EVENT_CATEGORY(EventCategoryMouse | EventCategoryInput)
+
+private:
+	float mouseX, mouseY;
+};
+
+class MouseScrolledEvent : public Event
+{
+public:
+	MouseScrolledEvent(const float xOffset_, const float yOffset_) : xOffset(xOffset_), yOffset(yOffset_) {}
+
+	// Getters
+	float GetXOffset() const { return xOffset; }
+	float GetYOffset() const { return yOffset; }
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "MouseScrolledEvent: " << GetXOffset() << ", " << GetYOffset();
+		return ss.str();
+	}
+
+	EVENT_TYPE(MouseScrolled)
+	EVENT_CATEGORY(EventCategoryMouse | EventCategoryInput)
+
+private:
+	float xOffset, yOffset;
+};
+
+class MouseButtonEvent : public Event
+{
+public:
+	// Getter
+	MouseCode GetMouseButton() const { return button; }
+
+	EVENT_CATEGORY(EventCategoryMouse | EventCategoryInput)
+protected:
+	MouseButtonEvent(const MouseCode button_) : button(button_) {}
+	MouseCode button;
+};
+
+class MouseButtonPressedEvent : public MouseButtonEvent
+{
+public:
+	MouseButtonPressedEvent(const MouseCode button_) : MouseButtonEvent(button) {}
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "MouseButtonPressedEvent: " << button;
+		return ss.str();
+	}
+
+	EVENT_TYPE(MouseButtonPressed)
+};
+
+class MouseButtonReleasedEvent : public MouseButtonEvent
+{
+	MouseButtonReleasedEvent(const MouseCode button_) : MouseButtonEvent(button) {}
+
+	std::string ToString() const override
+	{
+		std::stringstream ss;
+		ss << "MouseButtonReleasedEvent: " << button;
+		return ss.str();
+	}
+
+	EVENT_TYPE(MouseButtonReleased)
+};
 #endif
