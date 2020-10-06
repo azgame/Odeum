f8587c35515791edcff4f61615400b43ec17ee83
azgame <azizysgame@gmail.com>
2020-10-06 11:27:47 -0400

    texture resources

--- Odeum/OdeumEngine/OdeumEngine/Engine/Rendering/DirectX12/TextureManager.cpp
+++ Odeum/OdeumEngine/OdeumEngine/Engine/Rendering/DirectX12/TextureManager.cpp
@@ -1,1 +1,9 @@
 #include "TextureManager.h"
+
+void Texture::Create(size_t pitch_, size_t width_, size_t height_, DXGI_FORMAT format_, const void* initialData_)
+{
+}
+
+void Texture::CreateFromMemory(const void* bufferEntry_, size_t fileSize_)
+{
+}
--- Odeum/OdeumEngine/OdeumEngine/Engine/Rendering/DirectX12/TextureManager.h
+++ Odeum/OdeumEngine/OdeumEngine/Engine/Rendering/DirectX12/TextureManager.h
@@ -1,5 +1,55 @@
-#pragma once
-class TextureManager
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include "D3DIncludes.h"
+#include "Buffers/D3DResource.h"
+
+class Texture : public D3DResource
 {
+	friend class CommandContext;
+
+public:
+
+	Texture(const std::string& fileName_) 
+	{
+		m_textureKey = fileName_;
+		m_cpuDescHandle.ptr == D3D12_GPU_VIRTUAL_ADDRESS_UNKNOWN; 
+	}
+
+	Texture(const std::string& fileName_, D3D12_CPU_DESCRIPTOR_HANDLE handle) : m_cpuDescHandle(handle), m_textureKey(fileName_) {}
+
+	void Create(size_t pitch_, size_t width_, size_t height_, DXGI_FORMAT format_, const void* initialData_);
+	void Create(size_t width_, size_t height_, DXGI_FORMAT format_, const void* initialData_)
+	{
+		Create(width_, width_, height_, format_, initialData_);
+	}
+
+	void CreateFromMemory(const void* bufferEntry_, size_t fileSize_);
+
+	virtual void Destroy() override
+	{
+		D3DResource::Destroy();
+		m_cpuDescHandle.ptr = 0;
+	}
+
+	const D3D12_CPU_DESCRIPTOR_HANDLE& GetSRV() const { return m_cpuDescHandle; }
+
+private:
+
+	std::string m_textureKey;
+	D3D12_CPU_DESCRIPTOR_HANDLE m_cpuDescHandle;
 };
 
+class TextureManager
+{
+public:
+	static TextureManager* Get();
+	TextureManager(const TextureManager&) = delete;
+	TextureManager(TextureManager&&) = delete;
+	TextureManager& operator=(const TextureManager&) = delete;
+	TextureManager& operator=(TextureManager&&) = delete;
+
+
+};
+
+#endif
