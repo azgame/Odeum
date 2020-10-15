2a7ffa80d4b874fe479747c1455050be6d1f1d1f
Robert <robert.blank.wardo@gmail.com>
2020-10-15 19:52:53 -0400

    adjusted

--- Odeum/OdeumEngine/OdeumEngine/Game/Components/DynamicMovement.cpp
+++ Odeum/OdeumEngine/OdeumEngine/Game/Components/DynamicMovement.cpp
@@ -2,12 +2,18 @@
 void DynamicMovement::OnAttach(GameObject* parent)
 {
 	object = parent;
+	if (object->GetComponent<Rigidbody>()) {}
+	else { object->AddComponent<Rigidbody>(); }
+
+
+
+
 	//change these values later these are placeholders, should be set depending on the object
-	 maxAcceleration=5;
-	 maxSpeed=5;
-	 targetRadius=1.0f;
-	 slowRadius=10;
-	 timeToTarget=0.1;
+	maxAcceleration = 5;
+	maxSpeed = 5;
+	targetRadius = 3.0f;
+	slowRadius = 6;
+	timeToTarget = 0.1f;
 }
 
 void DynamicMovement::Update(float deltaTime)
@@ -15,29 +21,47 @@ void DynamicMovement::Update(float deltaTime)
 	//requires target to work
 	if (target)
 	{
-		if(!shouldFlee)
+
+
 		object->GetComponent<Rigidbody>()->SetAcceleration(GetSteering());
-		else
-		object->GetComponent<Rigidbody>()->SetAcceleration(-GetSteering());
-		
-			
+
+
+
 	}
 }
-
+void DynamicMovement::SetAll(float maxAcceleration_, float maxSpeed_, float targetRadius_, float slowRadius_, float timeToTarget_)
+{
+	maxAcceleration = maxAcceleration_; maxSpeed = maxSpeed_; targetRadius = targetRadius_; slowRadius = slowRadius_; timeToTarget = targetRadius_;
+}
 Vector4 DynamicMovement::GetSteering()
 {
 	Vector3 temp;
 	Vector3 direction;
 	//direction=target position-object position
-	direction.SetX(target->GetPosition().GetX() - object->GetPosition().GetX());
-	direction.SetY(target->GetPosition().GetY() - object->GetPosition().GetY());
-	direction.SetZ(target->GetPosition().GetZ() - object->GetPosition().GetZ());
+	if (!shouldFlee)
+	{
+		direction.SetX(target->GetPosition().GetX() - object->GetPosition().GetX());
+		direction.SetY(target->GetPosition().GetY() - object->GetPosition().GetY());
+		direction.SetZ(target->GetPosition().GetZ() - object->GetPosition().GetZ());
+	}
+	else
+	{
+		direction.SetX(object->GetPosition().GetX() - target->GetPosition().GetX());
+		direction.SetY(object->GetPosition().GetY() - target->GetPosition().GetY());
+		direction.SetZ(object->GetPosition().GetZ() - target->GetPosition().GetZ());
+	}
 	float distance = direction.Mag();
+
 	float targetSpeed;
 	//stop acclerating when in range
-	if (distance < targetRadius)
+	if (distance < targetRadius && !shouldFlee)
 	{
-		return Vector4(0.0f, 0.0f, 0.0f, 0.0f);
+
+		object->GetComponent<Rigidbody>()->SetVelocity(Vector4(0.0f, 0.0f, 0.0f, 0.0f));
+		return(Vector4(0.0f, 0.0f, 0.0f, 0.0f));
+
+
+
 	}
 	//if not in the slow range go full force otherwise slow down
 	if (distance > slowRadius)
@@ -48,9 +72,9 @@ Vector4 DynamicMovement::GetSteering()
 	{
 		targetSpeed = maxSpeed * distance / slowRadius;
 	}
-	
-	direction=direction.Normalize();
-	direction=direction * targetSpeed;
+
+	direction = direction.Normalize();
+	direction = direction * targetSpeed;
 	temp = direction - object->GetComponent<Rigidbody>()->GetVelocity();
 	temp = temp / timeToTarget;
 	// Clip acceleration if too high
@@ -60,5 +84,5 @@ Vector4 DynamicMovement::GetSteering()
 		temp = temp * maxAcceleration;
 	}
 
-	return Vector4(temp.GetX(),0.0f,temp.GetZ(), 0.0f);
+	return Vector4(temp.GetX(), 0.0f, temp.GetZ(), 0.0f);
 }
