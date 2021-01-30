5e8f4cf4a2ed23ebfef229881fb9204a6309010e
Robert <robert.blank.wardo@gmail.com>
2021-01-30 14:21:29 -0500

    Mash game

--- Odeum/OdeumEngine/OdeumEngine/Game/Scenes/ButtonMashingScene.cpp
+++ Odeum/OdeumEngine/OdeumEngine/Game/Scenes/ButtonMashingScene.cpp
@@ -6,13 +6,22 @@
 
 ButtonMashingScene::ButtonMashingScene()
 {
+	MaxPlayers = 4;
 	OdeumEngine::Get().GetCamera().SetPosition(Vector3(0.0f, 10.0f, -25.0f));
+	for (int i = 0; i < MaxPlayers; i++)
+	{
+		gameObjects.push_back(new GameObject(ShapeTypes::CubeShape, Colour(i, i, i)));
+		gameObjects.back()->AddComponent<Rigidbody>();
+		gameObjects.back()->GetComponent<Rigidbody>()->SetPosition(Vector4(i, -10, 0, 0));
+		playerPressed.push_back(false);
+	}
+	
+	
 
-	gameObjects.push_back(new GameObject(ShapeTypes::CubeShape, Colour(1.0, 0.2, 0.2)));
-	gameObjects.push_back(new GameObject(ShapeTypes::CubeShape, Colour(1.0, 1.0, 0.2)));
-	gameObjects.back()->AddComponent<Rigidbody>();
-	gameObjects.back()->GetComponent<Rigidbody>()->SetPosition(Vector4(kZero));
-
+	playerKeys.push_back(Key::H);
+	playerKeys.push_back(Key::J);
+	playerKeys.push_back(Key::K);
+	playerKeys.push_back(Key::L);
 	SceneGraph::Get()->LoadObjectsIntoMemory();
 	SceneGraph::Get()->UpdateObjects(0.0167f);
 }
@@ -29,11 +38,27 @@ bool ButtonMashingScene::Initialize()
 void ButtonMashingScene::Update(const float deltaTime_)
 {
 	SceneGraph::Get()->UpdateObjects(deltaTime_);
-
-	if (Input::Get().isKeyPressed(Key::K))
+	//gameObjects.back()->GetComponent<Rigidbody>()->
+	//gameObjects.back()->GetComponent<Rigidbody>()->ApplyForce(Vector4(0, -0.05, 0, 0));
+	for (int i = 0; i < gameObjects.size(); i++)
 	{
-		gameObjects.back()->GetComponent<Rigidbody>()->ApplyForce(Vector4(kYUnitVector));
+		gameObjects.at(i)->GetComponent<Rigidbody>()->AddVelocity(Vector4(0, -0.005, 0, 0));
+		if (Input::Get().isKeyPressed(playerKeys.at(i)))
+		{
+			if (!playerPressed.at(i))
+				gameObjects.at(i)->GetComponent<Rigidbody>()->AddVelocity(Vector4(0, 1.5f, 0, 0));
+			playerPressed.at(i) = true;
+			
+		}
+		else
+		{
+			playerPressed.at(i) = false;
+		}
+
 	}
+
+	
+	
 
 	//OdeumEngine::Get().GetCamera().SetPosition(Vector3(gameObjects.back()->GetComponent<Rigidbody>()->GetPosition()) + Vector3(0.0f, 10.0f, -25.0f));
 	/*OdeumEngine::Get().GetCamera().SetLookDirection( 
@@ -46,11 +71,18 @@ void ButtonMashingScene::UIRender()
 {
 	ImGui::Begin("Game UI");
 	ImGui::Text("Enter game UI components here");
-
-	if (gameObjects.back()->GetComponent<Rigidbody>()->GetPosition().GetY() > 25.0f)
+	for (int i = 0; i < gameObjects.size(); i++)
 	{
-		ImGui::Text("You win!");
+		if (gameObjects.at(i)->GetComponent<Rigidbody>()->GetPosition().GetY() > 25.0f)
+		{
+			std::string text = "Player "+i;
+			const char* textchar = text.c_str();
+			
+			ImGui::Text(textchar);
+		}
+		
 	}
+	
 
 	ImGui::End();
 }
--- Odeum/OdeumEngine/OdeumEngine/Game/Scenes/ButtonMashingScene.h
+++ Odeum/OdeumEngine/OdeumEngine/Game/Scenes/ButtonMashingScene.h
@@ -10,13 +10,16 @@ class ButtonMashingScene : public Scene
 public:
 	ButtonMashingScene();
 	virtual ~ButtonMashingScene();
-
+	int MaxPlayers;
+	 
 	virtual bool Initialize();
 	virtual void Update(const float deltaTime_);
 	virtual void UIRender();
 
 private:
 	std::vector<GameObject*> gameObjects;
+	std::vector<Key::KeyCode> playerKeys;
+	std::vector<bool> playerPressed;
 };
 
 #endif
