4753be8c4dcca487f3e9cbd2910ebf58e9ec9399
Ryan Thorne <rymaster_109@hotmail.com>
2021-02-06 18:06:51 -0500

    fixed errors

--- Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
+++ Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
@@ -4,8 +4,13 @@
 #include "../../pch.h"
 #include "../../Engine/Core/Component.h"
 
-class Player : public Component{
-
-}
+class Player : public Component {
+public:
+	// overridden functions
+	void OnAttach(GameObject* parent) override;
+	void OnDetach() {};
+	void Update(float deltaTime) override;
+private:
+};
 
 #endif
