67b76d7e5fadf1ce221d3188f2ca8975a579ebb1
Ryan Thorne <rymaster_109@hotmail.com>
2021-02-06 18:11:42 -0500

    keys

--- Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
+++ Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
@@ -3,6 +3,7 @@
 
 #include "../../pch.h"
 #include "../../Engine/Core/Component.h"
+#include "../../Engine/Core/OdeumEngine.h"
 
 class Player : public Component {
 public:
@@ -10,7 +11,15 @@ public:
 	void OnAttach(GameObject* parent) override;
 	void OnDetach() {};
 	void Update(float deltaTime) override;
+
+	// getters
+	inline std::vector<Key::KeyCode> GetKey() { return playerKeys; };
+
+	// setters
+	inline void AddKey(Key::KeyCode key) { playerKeys.push_back(key); };
+
 private:
+	std::vector<Key::KeyCode> playerKeys;
 };
 
 #endif
