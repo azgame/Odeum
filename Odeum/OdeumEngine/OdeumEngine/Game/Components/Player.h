dbee31f23c1be52bf9ffdc7478e067d25588817d
Ryan Thorne <rymaster_109@hotmail.com>
2021-02-28 17:37:18 -0500

    key stuff

--- Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
+++ Odeum/OdeumEngine/OdeumEngine/Game/Components/Player.h
@@ -15,16 +15,24 @@ public:
 	void OnStart() override;
 	void Update(float deltaTime) override;
 
-	// getters
+	// more complicated way to use keys.
 	inline std::vector<Key::KeyCode> GetKey() { return playerKeys; };
-
-	// setters
 	inline void AddKey(Key::KeyCode key) { playerKeys.push_back(key); };
+
+	// more simple way to set up keys.
+	inline void SetUp(Key::KeyCode key) { up = key; };
+	inline void SetDown(Key::KeyCode key) { down = key; };
+	inline void SetRight(Key::KeyCode key) { left = key; };
+	inline void SetLeft(Key::KeyCode key) { right = key; };
+	inline void SetJump(Key::KeyCode key) { jump = key; };
+	inline void SetInteract(Key::KeyCode key) { interact = key; };
 
 	void TakeDamage(double Damage);
 
 private:
 	std::vector<Key::KeyCode> playerKeys;
+
+	Key::KeyCode up, down, left, right, jump, interact;
 
 	StatComponent* stats;
 };
